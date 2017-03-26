23296f01dc9217c9e4a3c9a1085e518d8159276f
Ziming Qi <qiziming1@gmail.com>
2017-03-26 04:19:37 -0400

    added comparison

--- speech_recognition/__main__.py
+++ speech_recognition/__main__.py
@@ -1,28 +1,36 @@
 import speech_recognition as sr
-
+import random
 r = sr.Recognizer()
 m = sr.Microphone()
-
+keepGoing=True
+WORDS = "how,pick,random,word,from,long,list,apple,something,can,think,mike,banana,camera"
+WORDS=WORDS.split(",")
 try:
     print("A moment of silence, please...")
     with m as source: r.adjust_for_ambient_noise(source)
     print("Set minimum energy threshold to {}".format(r.energy_threshold))
-    while True:
-        print("Say something!")
-        with m as source: audio = r.listen(source)
-        print("Got it! Now to recognize it...")
-        try:
-            # recognize speech using Google Speech Recognition
-            value = r.recognize_google(audio)
 
-            # we need some special handling here to correctly print unicode characters to standard output
-            if str is bytes:  # this version of Python uses bytes for strings (Python 2)
-                print(u"You said {}".format(value).encode("utf-8"))
-            else:  # this version of Python uses unicode for strings (Python 3+)
-                print("You said {}".format(value))
-        except sr.UnknownValueError:
-            print("Oops! Didn't catch that")
-        except sr.RequestError as e:
-            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+    while keepGoing:
+        word=WORDS[random.randrange(0,len(WORDS))]
+        b = raw_input("-------------------------------Start? y/n---------------------------------------")
+        if b=="y":
+            print("Say "+word+"!")
+            with m as source: audio = r.listen(source)
+            print("Got it! Now to recognize it...")
+            try:
+                # recognize speech using Google Speech Recognition
+                value = str(r.recognize_google(audio)).lower()
+                print("You said: "+value)
+                if value==word:
+                    print("Speech Recgnition:PASS!")
+                else:
+                    print("Speech Recgnition:FAIL!")
+            except sr.UnknownValueError:
+                print("Oops! Didn't catch that")
+            except sr.RequestError as e:
+                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        else:
+            keepGoing=False
+            break
 except KeyboardInterrupt:
     pass
